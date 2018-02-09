0be1da7e65e7b2a3d844e871e81f5637145ef11d
Parvex <parvezl123@gmail.com>
2018-02-09 03:53:47 +0100

    completed whole app with zooming feature, in future i may improve algorithm to be more effecient or implement smooth coloring

---
+++ mandelbrot/MandelbrottZoomApp/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Drawing;
+using mandelbrot;
+
+namespace MandelbrottZoomApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MandelbrotImage mandelbrott;
+        public MainWindow()
+        {
+            mandelbrott = new MandelbrotImage();
+            InitializeComponent();
+            set.Source = getSource(mandelbrott.getBitmap());
+        }
+
+
+        BitmapImage getSource(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+
+                return bitmapimage;
+            }
+        }
+
+        private void setCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            System.Windows.Point position = Mouse.GetPosition(set);
+
+            mandelbrott.centerAndZoom(position.X, position.Y);
+            set.Source = getSource(mandelbrott.getBitmap());
+        }
+    }
+}
---
+++ mandelbrot/MandelbrottZoomApp/MandelbrotImage.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.Drawing;
+
+
+namespace mandelbrot
+{
+    class MandelbrotImage
+    {
+        Dictionary<int, Color> colors = new Dictionary<int, Color>();
+        Bitmap bitmap;
+        //size of the image
+        int bmpXmax = 1024;
+        int bmpYmax = 1024;
+        //domain in which mandelbrott set is being tested  
+        double xmin = -2.2;
+        double xmax = 1.2;
+        double ymin = -1.7;
+        double ymax = 1.7;
+        double height;
+        double width;
+        //number of iterations before accepting point
+        int iter_max = 150;
+
+        int zoom = 4;
+
+
+        //picking random colors for every iter break numbers
+        private void generateColors()
+        {
+            for(int i = 0; i < iter_max; i++)
+            {
+                colors[i] = IntToColor(i);
+            }
+            colors[iter_max] = Color.Black;
+        }
+
+        //gradient color generator from 0 to iter_max
+        private Color IntToColor(int i)
+        {
+            Color[] colorz = new Color[] { Color.Black, Color.Blue, Color.Cyan, Color.Green, Color.Yellow, Color.Orange, Color.Red, Color.White, Color.Transparent };
+            float scaled = (float)(i) / iter_max * 7;
+            Color color0 = colorz[(int)scaled];
+            Color color1 = colorz[(int)scaled + 1];
+            float fraction = scaled - (int)scaled;
+
+            int resultR = (byte)((1 - fraction) * (float)color0.R + fraction * (float)color1.R);
+            int resultG = (byte)((1 - fraction) * (float)color0.G + fraction * (float)color1.G);
+            int resultB = (byte)((1 - fraction) * (float)color0.B + fraction * (float)color1.B);
+            return Color.FromArgb(resultR, resultG, resultB);
+        }
+
+        public MandelbrotImage()
+        {
+            height = ymax - ymin;
+            width = xmax - xmin;
+            generateColors();
+            bitmap = new Bitmap(bmpXmax, bmpYmax);
+            generateBitmap();
+        }
+
+        private void generateBitmap()
+        {
+
+            for (int x = 1; x < bmpXmax; ++x)
+                for (int y = 1; y < bmpYmax; ++y)
+                {
+                    double Xtemp;
+                    DecPoint point = scale(x, y);
+                    DecPoint z = new DecPoint(0, 0);
+                    int iter;
+                    for (iter = 0; iter < iter_max && z.Abs2() < 4; ++iter)
+                    {
+                        Xtemp = z.X;
+                        z.X = z.X * z.X - z.Y * z.Y + point.X;
+                        z.Y = 2 * Xtemp * z.Y + point.Y;
+                    }
+
+                    bitmap.SetPixel(x, y, colors[iter]);
+                }
+        }
+
+        //scales pixel to fit in mandelbrott domain
+        private DecPoint scale(double x, double y)
+        {
+            return new DecPoint( ((double)x / bmpXmax) * width + xmin, ((double)y / bmpYmax) * height + ymin);
+        }
+
+
+        public Bitmap getBitmap()
+        {
+            return bitmap;
+        }
+
+        class DecPoint
+        {
+            DecPoint() { }
+            public DecPoint(double x, double y) { X = x;  Y = y; }
+            public double X;
+            public double Y;
+
+            public double Abs2()
+            {
+                return X * X + Y * Y;
+            }
+        }
+
+        public void centerAndZoom(double x, double y)
+        {
+            DecPoint center = scale((double)x, (double)y);
+            height = height / zoom;
+            width = width / zoom;
+            xmin = center.X - width / zoom;
+            xmax = center.X + width / zoom;
+            ymin = center.Y - height / zoom;
+            ymax = center.Y + height / zoom;
+            generateBitmap();
+        }
+    }
+
+   
+
+}
